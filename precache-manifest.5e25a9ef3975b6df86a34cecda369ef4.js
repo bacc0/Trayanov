f87fb6437afa1664f94f7cdef4be83266b8d43d9
Veselin Trayanov <bacco7@me.com>
2019-11-30 01:01:47 +0000

    react

---
+++ precache-manifest.5e25a9ef3975b6df86a34cecda369ef4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9fdf5bbe8831eeb6651c530174d623a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9772f5aa3d44630e4a39",
+    "url": "/static/css/main.dfdd57d8.chunk.css"
+  },
+  {
+    "revision": "343c867fa842f8af287d",
+    "url": "/static/js/2.aba617d7.chunk.js"
+  },
+  {
+    "revision": "9772f5aa3d44630e4a39",
+    "url": "/static/js/main.4bb2f389.chunk.js"
+  },
+  {
+    "revision": "16b039a709573ae90d35",
+    "url": "/static/js/runtime-main.5c4741ff.js"
+  }
+]);
